8788f26853650e48792105247279c614cbbaff6b
Nico <nicoandrade@gmail.com>
2015-12-14 16:10:31 -0300

    1.0.2

---
+++ js/theme-customizer-top-buttons.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+	var $destination = $("#customize-info .accordion-section-title");
+ 	$destination.prepend('<a style="width: 80%; margin: 10px auto; display: block; text-align: center;" href="https://www.quemalabs.com/?utm_source=Reyl%20Lite%20Theme&utm_medium=More%20Themes%20Button&utm_campaign=Reyl%20Lite" class="button" target="_blank">{pro}</a>'.replace( '{pro}', topbtns.pro ) );
+});
